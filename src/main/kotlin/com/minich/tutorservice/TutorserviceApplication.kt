44127feba3747a66ca172c11d0ef46fa2b2695da
Nikolay1992167 <wulik1081@mail.ru>
2025-03-02 23:34:41 +0300

    init commit

---
+++ src/main/kotlin/com/minich/tutorservice/TutorserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.minich.tutorservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutorserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TutorserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/minich/tutorservice/TutorserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.minich.tutorservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutorserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
